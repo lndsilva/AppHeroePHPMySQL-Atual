f93aab90a7314198adc1e3de07d27dd8cc5855da
Laercio-pc <lndsilva@hotmail.com>
2020-06-02 16:21:51 -0300

    alteração bd

--- HeroApi/HeroApi/includes/Constants.php
+++ HeroApi/HeroApi/includes/Constants.php
@@ -1,11 +1,7 @@
 <?php 
 
-	/*
-	* Created by Belal Khan
-	* website: www.simplifiedcoding.net 
-	*/
-	
+
 	define('DB_HOST', 'localhost');
-	define('DB_USER', 'root');
-	define('DB_PASS', '');
+	define('DB_USER', 'ti94');
+	define('DB_PASS', '123456');
 	define('DB_NAME', 'android');
--- HeroApi/HeroApi/includes/DbOperation.php
+++ HeroApi/HeroApi/includes/DbOperation.php
@@ -2,27 +2,23 @@
  
 class DbOperation
 {
-    //Database connection link
+    
     private $con;
  
-    //Class constructor
+ 
     function __construct()
     {
-        //Getting the DbConnect.php file
+  
         require_once dirname(__FILE__) . '/DbConnect.php';
  
-        //Creating a DbConnect object to connect to the database
+     
         $db = new DbConnect();
  
-        //Initializing our connection link of this class
-        //by calling the method connect of DbConnect class
+
         $this->con = $db->connect();
     }
 	
-	/*
-	* The create operation
-	* When this method is called a new record is created in the database
-	*/
+	
 	function createHero($name, $realname, $rating, $teamaffiliation){
 		$stmt = $this->con->prepare("INSERT INTO heroes (name, realname, rating, teamaffiliation) VALUES (?, ?, ?, ?)");
 		$stmt->bind_param("ssis", $name, $realname, $rating, $teamaffiliation);
@@ -30,11 +26,7 @@ class DbOperation
 			return true; 
 		return false; 
 	}
-
-	/*
-	* The read operation
-	* When this method is called it is returning all the existing record of the database
-	*/
+		
 	function getHeroes(){
 		$stmt = $this->con->prepare("SELECT id, name, realname, rating, teamaffiliation FROM heroes");
 		$stmt->execute();
@@ -56,10 +48,7 @@ class DbOperation
 		return $heroes; 
 	}
 	
-	/*
-	* The update operation
-	* When this method is called the record with the given id is updated with the new given values
-	*/
+	
 	function updateHero($id, $name, $realname, $rating, $teamaffiliation){
 		$stmt = $this->con->prepare("UPDATE heroes SET name = ?, realname = ?, rating = ?, teamaffiliation = ? WHERE id = ?");
 		$stmt->bind_param("ssisi", $name, $realname, $rating, $teamaffiliation, $id);
@@ -69,10 +58,6 @@ class DbOperation
 	}
 	
 	
-	/*
-	* The delete operation
-	* When this method is called record is deleted for the given id 
-	*/
 	function deleteHero($id){
 		$stmt = $this->con->prepare("DELETE FROM heroes WHERE id = ? ");
 		$stmt->bind_param("i", $id);
